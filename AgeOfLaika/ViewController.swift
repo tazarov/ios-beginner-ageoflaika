fa0f81cdeb0773b99a5f8dbfe998396f1c5b032b
Trayan Azarov <trayan.azarov@chaoslabs.bg>
2015-01-25 20:56:13 +0200

    Challenge completed

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let dogYearsConvertionConstant = 7
+        let humanYearsFromText = enterHumanYearsTextField.text.toInt()!
+        // this throws an exception when text is empty
+        dogYearsLabel.text = "\(dogYearsConvertionConstant * humanYearsFromText)" + " in dog years"
+        dogYearsLabel.textColor=UIColor.redColor()
+        dogYearsLabel.hidden=false
+        
+        enterHumanYearsTextField.text=""
+        enterHumanYearsTextField.resignFirstResponder()
+
+    }
 
 }
 
